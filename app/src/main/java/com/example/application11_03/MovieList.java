ef78a75159dadc49eb9e35093d30e76d8c8326ad
user <kk1204>
2021-11-24 02:27:59 +0900

    init_11st_app

---
+++ app/src/main/java/com/example/application11_03/MovieList.java
@@ -0,0 +1,5 @@
+package com.example.application11_03;
+
+public class MovieList {
+    MovieListResult boxOfficeResult;
+}
---
+++ app/src/main/java/com/example/application11_03/MovieListResult.java
@@ -0,0 +1,11 @@
+package com.example.application11_03;
+
+import java.util.ArrayList;
+import android.graphics.Movie;
+
+public class MovieListResult {
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
